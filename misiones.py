450dece698a79087d2411fe5cec87474f77d86e8
RonaldoOviedo19 <ronaldito456@gmail.com>
2024-08-05 00:04:33 -0500

    build: :boom: Se añade opciones nuevas a la seccion de misiones
    Se implementan nuevas opciones como la de asignar una mision a un ninja y la de mostrar las misiones para asi saber que misiones hay disponibles para estos ninjas
    
    @RonaldoOviedo19

--- misiones.py
+++ misiones.py
@@ -19,3 +19,45 @@ def agregarnueva_mision():
     print("------------ MISION AGREGADA EXITOSAMENTE ------------")
     print("------------------------------------------------------")
 
+def asignarmision():
+    print("-----------------------------------------------")
+    while True:
+        print("--------------------------------------------------------")
+        opc = menumision()
+        if opc == len(misiones)+1:
+            print("Saliendo...")
+            break    
+        elif opc == 1:
+            mis = misiones[0]
+            break
+        elif opc == 2:
+            mis = misiones[1]
+            break
+        elif opc == 3:
+            mis = misiones[2]
+            break
+        elif opc == 4:
+            mis = misiones[3]
+            break
+        elif opc == 5:
+            mis = misiones[4]
+            break
+        elif opc == 6:
+            mis = misiones[5]
+            break
+        elif opc == 7:
+            mis = misiones[6]
+            break
+        elif opc == 8:
+            mis = misiones[7]
+            break
+    print("------------------------------------------------------")
+    print("------------ MISION ASIGNADA EXITOSAMENTE ------------")
+    print("------------------------------------------------------")
+    return mis
+
+def mostrarmisiones():
+    print("LAS MISIONES DISPONIBLES SON : ")
+    print("")
+    for valor in misiones:
+        print(valor)
