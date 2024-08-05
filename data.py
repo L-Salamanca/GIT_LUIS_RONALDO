d476e7d274f6bdad9af6c2c994d0acaf35955a83
RonaldoOviedo19 <ronaldito456@gmail.com>
2024-08-04 23:58:50 -0500

    build: :sparkles: Se añade la opcion de cargar datos
    En esta parte despues de guardar los datos los cargamos para asi tenerlos siempre a la mano en el archivo y no estar digitando de nuevo toda la informacion
    
    @RonaldoOviedo19

--- data.py
+++ data.py
@@ -4,3 +4,9 @@ def guardar_datos(data, filename='ninjas.json'):
     with open(filename, 'w') as file:
         json.dump(data, file, indent=4)
 
+def cargar_datos(filename='ninjas.json'):
+    try:
+        with open(filename, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
